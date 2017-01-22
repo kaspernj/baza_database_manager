962159e20467754aaf3dc7456a95307ab0e11e52
kaspernj <kaspernj@gmail.com>
2017-01-22 17:41:14 +0100

    Added support for creating and dropping foreign keys

--- app/controllers/foreign_keys_controller.rb
+++ app/controllers/foreign_keys_controller.rb
@@ -7,10 +7,34 @@ class ForeignKeysController < ApplicationController
     end
   end
 
-  def new; end
+  def new
+    with_db do
+      render
+    end
+  end
 
   def create
-    raise "stub"
+    with_db do
+      begin
+        local_column = @table.column(foreign_key_params[:local_column_name])
+        table = @db.tables[foreign_key_params[:table_name]]
+        column = table.column(foreign_key_params[:column_name])
+
+        local_column.create_foreign_key(
+          name: foreign_key_params[:name],
+          column: column
+        )
+
+        flash[:success] = controller_t(".the_foreign_key_was_created")
+        redirect_to [@profile, @database, @table]
+      rescue Baza::Errors::TableNotFound
+        flash[:error] = controller_t(".no_such_table", table_name: foreign_key_params[:table_name])
+        render :new
+      rescue Baza::Errors::ColumnNotFound
+        flash[:error] = controller_t(".no_such_column", column_name: foreign_key_params[:column_name])
+        render :new
+      end
+    end
   end
 
   def edit; end
@@ -20,6 +44,15 @@ class ForeignKeysController < ApplicationController
   end
 
   def destroy
-    raise "stub"
+    with_db do
+      @foreign_key.drop
+      redirect_to [@profile, @database, @table]
+    end
+  end
+
+private
+
+  def foreign_key_params
+    params.require(:foreign_key).permit(:name, :local_column_name, :table_name, :column_name)
   end
 end
