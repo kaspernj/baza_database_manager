b2b3a58fafc86e58a5a65d4d63456733c40a0394
kaspernj <kaspernj@gmail.com>
2017-01-27 10:05:50 +0100

    Added more specs for exports

---
+++ spec/factories/export.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :export do
+    database_name "Main"
+  end
+end
--- spec/features/exports_spec.rb
+++ spec/features/exports_spec.rb
@@ -1,30 +1,96 @@
 require "rails_helper"
 
 describe ExportsController do
+  let(:export) { create :export, profile: profile }
   let(:user) { create :user }
   let!(:profile) { create :profile }
   let(:db_inst) { profile.database_instance }
   let(:table) { db_inst.tables["test_table"] }
   let(:db) { db_inst.databases["Main"] }
 
-  before do
-    login_as user
+  describe "#show" do
+    it "renders the page" do
+      login_as user
+
+      visit profile_database_export_path(profile, db, export)
+
+      expect(page).to have_http_status :success
+      expect(current_path).to eq profile_database_export_path(profile, db, export)
+    end
+
+    it "exports sql" do
+      login_as user
+
+      visit profile_database_export_path(profile, db, export, format: :sql)
+
+      expect(page).to have_http_status :success
+      expect(current_path).to eq profile_database_export_path(profile, db, export, format: :sql)
+      expect(page.response_headers["Content-Disposition"]).to eq "attachment; filename=\"Main.sql.gz\""
+    end
   end
 
-  it "#new" do
-    visit new_profile_database_export_path(profile, db)
-    expect(page).to have_http_status(:success)
-    expect(current_path).to eq visit new_profile_database_export_path(profile, db)
+  describe "#new" do
+    it "renders the page" do
+      login_as user
+
+      visit new_profile_database_export_path(profile, db)
+
+      expect(page).to have_http_status :success
+      expect(current_path).to eq new_profile_database_export_path(profile, db)
+    end
   end
 
-  it "#create" do
-    visit new_profile_database_export_path(profile, db)
+  describe "#create" do
+    it "creates a new export" do
+      login_as user
 
-    expect { find("input[type=submit]").click }.to change(Export, :count).by(1)
+      visit new_profile_database_export_path(profile, db)
 
-    created_export = Export.last
+      expect { find("input[type=submit]").click }.to change(Export, :count).by(1)
 
-    expect(page).to have_http_status(:success)
-    expect(current_path).to eq visit profile_database_export_path(profile, db, created_export)
+      created_export = Export.last
+
+      expect(page).to have_http_status :success
+      expect(current_path).to eq profile_database_export_path(profile, db, created_export)
+    end
+  end
+
+  describe "#edit" do
+    it "renders the page" do
+      login_as user
+
+      visit edit_profile_database_export_path(profile, db, export)
+
+      expect(page).to have_http_status :success
+      expect(current_path).to eq edit_profile_database_export_path(profile, db, export)
+    end
+  end
+
+  describe "#update" do
+    it "updates the given export" do
+      login_as user
+
+      visit edit_profile_database_export_path(profile, db, export)
+
+      find("input[type=submit]").click
+
+      expect(page).to have_http_status :success
+      expect(current_path).to eq profile_database_export_path(profile, db, export)
+    end
+  end
+
+  describe "#destroy" do
+    it "deletes the export" do
+      login_as user
+
+      visit profile_database_export_path(profile, db, export)
+
+      expect do
+        find(".btn-danger").click
+      end.to change(Export, :count).by(-1)
+
+      expect(page).to have_http_status :success
+      expect(current_path).to eq profile_database_path(profile, db)
+    end
   end
 end
